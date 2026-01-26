84a1cb1ec016212db1d74645e136234f146426fb
potatochi030 <oqdehaysha@gmail.com>
2026-01-26 18:50:45 +0300

    norminette

--- cmd.c
+++ cmd.c
@@ -1,97 +1,98 @@
 #include "pipex.h"
 
-void    *safty_check(char *ptr)
+void	*safty_check(char *ptr)
 {
-    if (ptr == NULL)
-    {
-        perror("Malloc ERROR:");
-        exit(-1);
-    }
-    return (ptr);
+	if (ptr == NULL)
+	{
+		perror("Malloc ERROR:");
+		exit(-1);
+	}
+	return (ptr);
 }
 
-char    **get_cmd_args(char *cmd)
+char	**get_cmd_args(char *cmd)
 {
-    char    **args;
+	char	**args;
 
-    args = ft_split(cmd, ' ');
-    if (!args)
-    {
-        perror("Malloc ERROR:");
-        exit (-1);
-    }
-    return (args);
+	args = ft_split(cmd, ' ');
+	if (!args)
+	{
+		perror("Malloc ERROR:");
+		exit (-1);
+	}
+	return (args);
 }
 
-char    *get_PATH(char **envp)
+char	*get_path(char **envp)
 {
-    int     i;
-    int     j;
-    char    *variable;
+	int		i;
+	int		j;
+	char	*variable;
 
-    i = 0;
-    while (envp[i])
-    {
-        j = 0;
-        while (envp[i][j] && envp[i][j] != '=')
-            j++;
-        variable = safty_check(ft_substr(envp[i], 0, j));
-        if (ft_strncmp(variable, "PATH", 4) == 0)
-        {
-            free(variable);
-            return (envp[i] + j + 1);
-        }
-        free(variable);
-        i++;
-    }
-    return (NULL);
-}
-char    *get_cmd_path (char *cmd, char **envp)
-{
-    int     i;
-    char    **search;
-    char    *prefix;
-    char    *cmd_path;
-
-    if (cmd == NULL)
-        return (NULL);
-    if (access(cmd, F_OK | X_OK) == 0)
-        return (ft_strdup(cmd));
-    search = ft_split(get_PATH(envp), ':');
-    i  = 0;
-    while (search && search[i])
-    {
-        prefix = ft_strjoin(search[i], "/");
-        cmd_path = ft_strjoin(prefix, cmd);
-        free(prefix);
-        if (access(cmd_path, F_OK | X_OK) == 0)
-            return (cmd_path);
-        free(cmd_path);
-        i++;
-    }
-    if (search)
-        free_me_from_agony(search);
-    return (NULL);
+	i = 0;
+	while (envp[i])
+	{
+		j = 0;
+		while (envp[i][j] && envp[i][j] != '=')
+			j++;
+		variable = safty_check(ft_substr(envp[i], 0, j));
+		if (ft_strncmp(variable, "PATH", 4) == 0)
+		{
+			free(variable);
+			return (envp[i] + j + 1);
+		}
+		free(variable);
+		i++;
+	}
+	return (NULL);
 }
 
-void    exec_cmd(char *cmd, char **envp)
+char	*get_cmd_path(char *cmd, char **envp)
 {
-    char    *path;
-    char    **args;
+	int		i;
+	char	**search;
+	char	*prefix;
+	char	*cmd_path;
 
-    args = get_cmd_args(cmd);
-    path = get_cmd_path(args[0], envp);
-    if (path == NULL)
-    {
-        free_me_from_agony(args);
-        perror("Malloc ERROR: ");
-        exit(-1);
-    }
-    if (execve(path, args, envp) == -1)
-    {
-        perror("Execve ERROR: ");
-        free(path);
-        free_me_from_agony(args);
-        exit(-1);
-    }
+	if (cmd == NULL)
+		return (NULL);
+	if (access(cmd, F_OK | X_OK) == 0)
+		return (ft_strdup(cmd));
+	search = ft_split(get_path(envp), ':');
+	i = 0;
+	while (search && search[i])
+	{
+		prefix = ft_strjoin(search[i], "/");
+		cmd_path = ft_strjoin(prefix, cmd);
+		free(prefix);
+		if (access(cmd_path, F_OK | X_OK) == 0)
+			return (cmd_path);
+		free(cmd_path);
+		i++;
+	}
+	if (search)
+		free_me_from_agony(search);
+	return (NULL);
+}
+
+void	exec_cmd(char *cmd, char **envp)
+{
+	char	*path;
+	char	**args;
+
+	args = get_cmd_args(cmd);
+	path = get_cmd_path(args[0], envp);
+	if (path == NULL)
+	{
+		free_me_from_agony(args);
+		perror("Malloc ERROR: ");
+		exit(-1);
+	}
+	if (execve(path, args, envp) == -1)
+	{
+		perror("Execve ERROR: ");
+		free(path);
+		free_me_from_agony(args);
+		exit(-1);
+	}
 }
--- main.c
+++ main.c
@@ -1,56 +1,57 @@
 #include "pipex.h"
 
-void    first_cmd(char  **argv, int  pipefd[2], char **envp)
+void	first_cmd(char **argv, int pipefd[2], char **envp)
 {
-    int     in_file;
+	int	in_file;
 
-    in_file = open(argv[1], O_RDONLY);
-    if (in_file == -1)
-    {
-        perror("File ERROR#1: ");
-        exit(-1);
-    }
-    close(pipefd[0]);
-    dup2(in_file, 0);
-    dup2(pipefd[1], 1);
-    close(in_file);
-    close(pipefd[1]);
-    exec_cmd(argv[2], envp);
+	in_file = open(argv[1], O_RDONLY);
+	if (in_file == -1)
+	{
+		perror("File ERROR#1: ");
+		exit(-1);
+	}
+	close(pipefd[0]);
+	dup2(in_file, 0);
+	dup2(pipefd[1], 1);
+	close(in_file);
+	close(pipefd[1]);
+	exec_cmd(argv[2], envp);
 }
 
-void    second_cmd(char  **argv, int  pipefd[2], char **envp)
+void	second_cmd(char **argv, int pipefd[2], char **envp)
 {
-    int     out_file;
-    out_file = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC);
-    if (out_file == -1)
-    {
-        perror("File ERROR#2: ");
-        exit(-1);
-    }
-    close(pipefd[1]);
-    dup2(out_file, 1);
-    dup2(pipefd[0], 0);
-    close(out_file);
-    close(pipefd[0]);
-    exec_cmd(argv[3], envp);
+	int	out_file;
+
+	out_file = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC);
+	if (out_file == -1)
+	{
+		perror("File ERROR#2: ");
+		exit(-1);
+	}
+	close(pipefd[1]);
+	dup2(out_file, 1);
+	dup2(pipefd[0], 0);
+	close(out_file);
+	close(pipefd[0]);
+	exec_cmd(argv[3], envp);
 }
 
-int     main(int argc, char **argv, char **envp)
+int	main(int argc, char **argv, char **envp)
 {
-    int     pipefd[2];
-    pid_t   pid;
-    
-    if (argc != 5)
-    {
-        write (2, "Pipex Usage: ./pipex infile cmd1 cmd2 outfile\n", 46);
-        return (0);
-    }
-    if (pipe(pipefd) == -1)
-        exit(-1);
-    pid = fork();
-    if (pid == -1)
-        exit(-1);
-    if (!pid)
-        first_cmd(argv, pipefd, envp);
-    second_cmd(argv, pipefd, envp);
+	int		pipefd[2];
+	pid_t	pid;
+
+	if (argc != 5)
+	{
+		write (2, "Pipex Usage: ./pipex infile cmd1 cmd2 outfile\n", 46);
+		return (0);
+	}
+	if (pipe(pipefd) == -1)
+		exit(-1);
+	pid = fork();
+	if (pid == -1)
+		exit(-1);
+	if (!pid)
+		first_cmd(argv, pipefd, envp);
+	second_cmd(argv, pipefd, envp);
 }
